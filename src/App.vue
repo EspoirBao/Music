c9302ec3fe0b26c5a6e3824115bd747d246673fc
EspoirBao <51221361+EspoirBao@users.noreply.github.com>
2020-01-16 20:33:09 +0800

    v0.2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/search">search</router-link>
+    <div class="banner">
+      <span class="L">网易云音乐</span>
+      <span class="R">下载app</span>
     </div>
-    <router-view/>
+    <div id="nav">
+      <van-tabs v-model="active">
+        <van-tab title="Home"></van-tab>
+        <van-tab title="About"></van-tab>
+        <van-tab title="search"></van-tab>
+        
+      </van-tabs>
+      
+      
+      
+    </div>
+    <router-view />
   </div>
 </template>
-
+<script>
+import Vue from "vue";
+export default {};
+</script>
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.banner{
+  height: 0.85rem;
+  width: 100%;
+  background: #d43c33;
+  display: flex;
+  justify-content: space-around;
+  padding: 0 0.1rem;
+  .span{
+    color: #fff;
+    font-size: 24px;
   }
+  .L{
+    
+  }
+  .R{
+
+  }
+
+}
+#nav {
+  
+  
+ 
 }
 </style>
